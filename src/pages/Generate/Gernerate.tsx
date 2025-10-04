30de66fc87e105020dddb5d11827df2bdd21f6dd
chsd0 <serzer77@yandex.ru>
2025-10-04 14:43:45 +0300

    commiting

--- src/pages/Generate/Gernerate.tsx
+++ src/pages/Generate/Gernerate.tsx
@@ -5,9 +5,11 @@ export const Generate = () => {
   const [open, setOpen] = useState(true);
 
   return (
-    <Dialog open={open}>
+    <Dialog open={open} maxWidth="lg" fullWidth>
       <DialogTitle>Subscribe</DialogTitle>
-      <DialogContent>Negr isasdas ready!</DialogContent>
+      <DialogContent>
+        <img  src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" alt="Loading..." />
+      </DialogContent>
     </Dialog>
   );
 };
