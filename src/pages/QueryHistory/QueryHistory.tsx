5e071659a66c44b7c509d59fcc3f842c7904c4db
KurenkovDmitry <67153890+KurenkovDmitry@users.noreply.github.com>
2025-11-08 06:09:22 +0300

    [fix] url

--- src/pages/QueryHistory/QueryHistory.tsx
+++ src/pages/QueryHistory/QueryHistory.tsx
@@ -3,8 +3,7 @@ import { QueryButton } from "./components/QueryButton";
 import { useEffect, useState } from "react";
 import { CircularProgress } from "@mui/material";
 import { DatasetDialog } from "./components/DatasetDialog";
-
-const API_BASE_URL = "http://127.0.0.1:8085/api/v1";
+import { API_BASE_URL } from "@services/api";
 
 interface DatasetsDTO {
   requestId: string;
--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8085/api/v1";
+import { API_BASE_URL } from "./config";
 
 class ApiClient {
   private baseURL: string;
